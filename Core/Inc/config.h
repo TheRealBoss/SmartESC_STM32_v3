7039f87fc17a8639a9bdd08653f35d4a47410abc
Kaur Viiklaid <kaurviiklaid@msn.com>
2024-04-26 08:53:58 +0300

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -21,24 +21,24 @@
 #define FIELD_WEAKNING_CURRENT_MAX 0 //max id
 
 // ADC channels
-#define ADC_VOLTAGE 0
-#define ADC_THROTTLE 1
-#define ADC_TEMP 2
+#define ADC_VOLTAGE 6
+#define ADC_THROTTLE 7
+#define ADC_TEMP 0
 
 #define LED_Pin GPIO_PIN_1
 #define LED_GPIO_Port GPIOD
-#define UART1_Tx_Pin GPIO_PIN_6
-#define UART1_Tx_GPIO_Port GPIOB
-#define BrakeLight_Pin GPIO_PIN_15
-#define BrakeLight_GPIO_Port GPIOA
+#define UART1_Tx_Pin GPIO_PIN_2
+#define UART1_Tx_GPIO_Port GPIOA
+#define BrakeLight_Pin GPIO_PIN_3
+#define BrakeLight_GPIO_Port GPIOB
 #define Temp_Pin GPIO_PIN_0
 #define Temp_GPIO_Port GPIOA
-#define Throttle_Pin GPIO_PIN_1
+#define Throttle_Pin GPIO_PIN_7
 #define Throttle_GPIO_Port GPIOA
-#define Batt_Voltage_Pin GPIO_PIN_2
+#define Batt_Voltage_Pin GPIO_PIN_6
 #define Batt_Voltage_GPIO_Port GPIOA
-#define PWR_BTN_Pin GPIO_PIN_14
-#define PWR_BTN_GPIO_Port GPIOC
-#define TPS_ENA_Pin GPIO_PIN_15
-#define TPS_ENA_GPIO_Port GPIOC
+#define PWR_BTN_Pin GPIO_PIN_12
+#define PWR_BTN_GPIO_Port GPIOA
+#define TPS_ENA_Pin GPIO_PIN_11
+#define TPS_ENA_GPIO_Port GPIOA
 
